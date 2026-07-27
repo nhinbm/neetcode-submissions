fb3741254597a196921c525e523067f64d5dab08
Nguyễn Bùi Mẫn Nhi <112642751+nhinbm@users.noreply.github.com>
2026-07-27 22:35:29 +0700

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let left = 0;
+        let right = matrix.length * matrix[0].length - 1;
+
+        console.log(left, right)
+
+        while (left <= right) {
+            let mid = left + Math.floor((right - left) / 2);
+            let row = Math.floor(mid / matrix[0].length); 
+            let col = mid % matrix[0].length;
+
+            if (target < matrix[row][col]) {
+                right -= 1;
+            } else if (target > matrix[row][col]) {
+                left += 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
