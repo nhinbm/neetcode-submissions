f246fbba1c9b43450386cfb6869250f4462c76ad
Nguyễn Bùi Mẫn Nhi <112642751+nhinbm@users.noreply.github.com>
2026-07-28 23:26:15 +0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        let freq = {};
+
+        for (let i = 0; i < s1.length; i++) {
+            if (!freq[s1[i]]) {
+                freq[s1[i]] = 0;
+            }
+            freq[s1[i]] += 1;
+        }
+
+        let left = 0;
+        let right = s1.length - 1;
+
+        console.log(freq)
+
+        while(right < s2.length) {
+            if (!freq[s2[left]]) {
+                left += 1;
+                right += 1;
+            } else {
+                let i = left;
+
+                while (freq[s2[i]]) {
+                    freq[s2[i]] -= 1;
+                    i += 1;
+                }
+
+                if (i > right) {
+                    return true;
+                }
+
+                while(i > left) {
+                    i -= 1;
+                    freq[s2[i]] += 1;
+                }
+
+                left += 1;
+                right += 1;
+            }
+        }
+
+        return false;
+    }
+}
