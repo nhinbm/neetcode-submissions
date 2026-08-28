394bb13015545090c46102c7ff35751b7de03c80
Nguyễn Bùi Mẫn Nhi <112642751+nhinbm@users.noreply.github.com>
2026-08-28 10:43:49 +0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let buy = prices[0];
+        let profit = 0;
+
+        for (let i = 1; i < prices.length; i++) {
+            buy = Math.min(buy, prices[i]);
+            profit = Math.max(profit, prices[i] - buy);
+        }
+
+        return profit;
+    }
+}
